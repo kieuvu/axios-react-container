9a22d5fec82ca5075f9b9f471846faf350304340
“kieuvu” <kieuvuvuvu@gmail.com>
2023-03-31 10:19:53 +0700

    bugfix

--- src/axios.js
+++ src/axios.js
@@ -1,31 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import instance from './request';
 
 const AxiosInterceptor = ({ children }) => {
+  const [isComplete, setIsComplete] = useState(false);
+
   useEffect(() => {
-    const resInterceptor = (response) => {
-      console.log("response", response);
-      return response;
-    };
-
-    const errInterceptor = (error) => {
-      console.log("error", error);
-      if (error.response.status === 401) {
-        //redirect logic here
-      }
-
-      return Promise.reject();
-    };
-
     const interceptor = instance.interceptors.response.use(
-      resInterceptor,
-      errInterceptor
+      response => {
+        console.log("response", response);
+        return response;
+      },
+      error => {
+        console.log("error", error);
+        // handle error here
+        return Promise.reject();
+      }
     );
+
+    setIsComplete(true);
 
     return () => instance.interceptors.response.eject(interceptor);
   }, []);
 
-  return children;
+  return isComplete && children;
 };
 
 export { AxiosInterceptor };
